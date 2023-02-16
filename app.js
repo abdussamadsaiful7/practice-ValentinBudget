53baf02785b4d673b974ac39b8ff424e84ecbb66
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-16 16:08:42 +0600

    added js code

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
         const kitKatCost = chocolateQuantity * 200;
         setInnerText('total-chocolate', kitKatCost);
         total();
+      
+
     });
       
       
@@ -16,9 +18,9 @@
     });
 
     document.getElementById('diary-buy-btn').addEventListener('click', function(){
-        const dairyQuantity = getInputValue('diary-quantity');
-        const dairyCost = dairyQuantity * 100;
-        setInnerText('total-diary', dairyCost);
+        const diaryQuantity = getInputValue('diary-quantity');
+        const diaryCost = diaryQuantity * 100;
+        setInnerText('total-diary', diaryCost);
         total();
     });
 
@@ -31,15 +33,35 @@
     function getInputValue(id,){
         const value = document.getElementById(id).value; 
         return parseInt(value);
+    
     };
+
+
 
     function total(params){
         const chocolate = document.getElementById('total-chocolate').innerText;
         const rose = document.getElementById('total-rose').innerText;
-        const dairy = document.getElementById('total-diary').innerText;
-     
-        const totalSum = parseInt(chocolate) + parseInt(rose) + parseInt(dairy); 
-        // document.getElementById('total-cost').innerText = totalSum;
+        const diary = document.getElementById('total-diary').innerText;
+
+        const totalSum = parseInt(chocolate) + parseInt(rose) + parseInt(diary); 
+        document.getElementById('total-cost').innerText = totalSum;
         setInnerText('total-cost', totalSum); 
 
     };
+
+
+    document.getElementById('promoApply-btn').addEventListener('click', function(){
+        const promoCode = getInputValue('promo-code');
+       if(promoCode === 101){
+        const total =  document.getElementById('total-cost').innerText;
+        const sum = total - parseInt(total) * 0.1;
+        setInnerText('all-total', sum);
+       }
+       else{
+        alert('Wrong promo Code, try again with valid code!');
+       };
+        
+    });
+
+
+    
---
+++ app2.js
@@ -0,0 +1,46 @@
+
+
+
+// document.getElementById('kitKat-buy-btn').addEventListener('click', function(){
+//     const chocolateQuantity = document.getElementById('kitKat-quantity').value;
+//     const chocolateQuantityCost = chocolateQuantity * 200;
+//     document.getElementById('total-chocolate').innerText = chocolateQuantityCost;
+//     total();
+    
+   
+// });
+
+// document.getElementById('rose-buy-btn').addEventListener('click', function(){
+//     const roseQuantity = document.getElementById('rose-quantity').value;
+//     const roseQuantityCost = roseQuantity * 100;
+//     document.getElementById('total-rose').innerText = roseQuantityCost;
+//     total();
+   
+   
+// });
+
+// document.getElementById('diary-buy-btn').addEventListener('click', function(){
+//     const diaryQuantity = document.getElementById('diary-quantity').value;
+//     const diaryQuantityCost = diaryQuantity * 100;
+//     document.getElementById('total-diary').innerText = diaryQuantityCost;
+//     total();
+  
+   
+// });
+
+// function total(){
+//     const chocolate = document.getElementById('total-chocolate').innerText;
+//     const rose = document.getElementById('total-rose').innerText;
+//     const diary = document.getElementById('total-diary').innerText;
+
+//     const totalSum = parseInt(chocolate) + parseInt(rose) + parseInt(diary); 
+//     document.getElementById('total-cost').innerText = totalSum;
+// };
+
+
+
+// document.getElementById('promoApply-btn').addEventListener('click', function(){
+//     const total =  document.getElementById('total-cost').innerText;
+//     const sum = total - parseInt(total) * 0.1;
+//     document.getElementById('all-total').innerText = sum;
+// });
